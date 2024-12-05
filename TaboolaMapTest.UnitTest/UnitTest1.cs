48513b9af514ea85f767cc5060558c0cfb5cbe16
Dav He <s991179@gmail.com>
2024-12-05 22:46:40 +0800

    feat: init

---
+++ TaboolaMapTest.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TaboolaMapTest.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
