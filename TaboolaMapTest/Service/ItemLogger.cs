1608b9d61aac33ae925a6a04602f1ab5a1f8f383
Dav He <s991179@gmail.com>
2024-12-06 09:56:20 +0800

    feat: add ItemLogger

---
+++ TaboolaMapTest/Service/ItemLogger.cs
@@ -0,0 +1,29 @@
+namespace TaboolaMapTest.Service;
+
+public class ItemLogger: IItemLogger
+{
+    private readonly Dictionary<int, Dictionary<string, long>> _map = new();
+    
+    public void LogItem(string item, long timeStamp)
+    {
+        var minute = DateTimeOffset.FromUnixTimeSeconds(timeStamp).Minute;
+
+        if (!_map.ContainsKey(minute))
+        {
+            _map[minute] = new Dictionary<string, long>();
+        }
+
+        if (!_map[minute].ContainsKey(item))
+        {
+            _map[minute][item] = timeStamp;
+            return;
+        }
+
+        _map[minute][item] = Math.Max(_map[minute][item], timeStamp);
+    }
+
+    public bool HasItemUpdatedSince(string item, long timeStamp)
+    {
+        return false;
+    }
+}
